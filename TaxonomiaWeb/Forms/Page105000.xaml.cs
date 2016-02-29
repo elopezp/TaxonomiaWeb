d6ff72df08f6fe291155499c2c331e1c805e4b4b
ericklaynus <laykiller88@hotmail.com>
2016-02-29 15:52:16 -0600

    Busy message

--- TaxonomiaWeb/Forms/Page105000.xaml.cs
+++ TaxonomiaWeb/Forms/Page105000.xaml.cs
@@ -306,6 +306,7 @@ namespace TaxonomiaWeb.Forms
                 servBmvXblr = new Service1Client();
                 servBmvXblr.SaveBmvReporteCompleted += servBmvXblr_SaveBmvReporteCompleted;
                 servBmvXblr.SaveBmvReporteAsync(sortedList, mainPage.Compania,  mainPage.IdAno, mainPage.IdTrimestre);
+                busyIndicator.IsBusy = true;
             //}
 
         }
@@ -389,6 +390,7 @@ namespace TaxonomiaWeb.Forms
 
         void servBmvXblr_SaveBmvReporteCompleted(object sender, SaveBmvReporteCompletedEventArgs e)
         {
+            busyIndicator.IsBusy = false;
             if (e.Error == null)
             {
                 bool res = (bool)e.Result;
